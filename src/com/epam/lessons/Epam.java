26edcb3ddfd006072eb759341e64868ad8054932
Liliia Volkova <liliia_volkova@outlook.com>
2019-02-18 22:54:39 +0200

    Initial commit

---
+++ src/com/epam/lessons/Epam.java
@@ -0,0 +1,11 @@
+package com.epam.lessons;
+
+public class Epam {
+    public static void main(String[] args) {
+        System.out.println("**** ****     *      *   *");
+        System.out.println("*    *  *    * *     ** **");
+        System.out.println("**   ****   *   *    * * *");
+        System.out.println("*    *     *******   *   *");
+        System.out.println("**** *    *       *  *   *");
+    }
+}
